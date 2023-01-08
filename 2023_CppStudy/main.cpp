bc975807f406dfb4edd4330887b9a4aa99ea6e91
JongWoo <jinlaove17@naver.com>
2023-01-08 14:34:43 +0900

    [Add] 자료형

--- 2023_CppStudy/main.cpp
+++ 2023_CppStudy/main.cpp
@@ -2,5 +2,13 @@
 
 int main()
 {
+	// 자료형(Data Type), 크기 단위: 바이트(byte)
+	// 정수형: char(1), short(2), int(4), long(4), long long(8)
+	// 실수형: float(4), double(8)
+	int i = 0;
+	unsigned char c = 0;
 
+	c = 0;
+	c = 255;
+	c = 256;
 }
