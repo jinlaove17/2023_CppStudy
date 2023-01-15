c0e9fa932805c2768da3a246157e1409b4eda03a
JongWoo <jinlaove17@naver.com>
2023-01-15 15:11:14 +0900

    [Add] 클래스

--- 2023_CppStudy/main.cpp
+++ 2023_CppStudy/main.cpp
@@ -1,106 +1,85 @@
 #include <iostream>
-#include <time.h>
 
-using namespace std;
+class CTest
+{
+	// 접근 제한 지정자: 클래스 내의 멤버 변수, 멤버 함수 등의 접근 레벨
+	// private, protected, public
+private:
+	int m_i;
 
-void Test();
-void Sort(int* arr, int n, void(*f)(int*, int));
-void SelectionSort(int* arr, int n);
-void InsertionSort(int* arr, int n);
-void BubbleSort(int* arr, int n);
+public:
+	// 생성자
+	CTest() :
+		m_i() // 초기화 리스트
+	{
+		printf("클래스 CTest의 생성자 호출\n");
+	}
+
+	// 소멸자
+	~CTest()
+	{
+		printf("클래스 CTest의 소멸자 호출\n");
+	}
+
+	// 대입 연산자
+	// - 반환 타입이 const&가 되어서는 안된다.(예시: (a = b) = c))
+	CTest& operator =(const CTest& other)
+	{
+		m_i = other.m_i;
+
+		return *this;
+	}
+
+	void SetInt(int i)
+	{
+		m_i = i;
+
+		// this 포인터: 이 멤버 함수를 호출시킨 객체(인스턴스)의 주소(생략 가능)
+		// // 다시 말해 이 함수를 void SetInt(CTest* this, int i); 처럼 만든 것과 같다.
+		// this->m_i = i;
+	}
+
+	int GetInt()
+	{
+		return m_i;
+	}
+};
 
 int main()
 {
-	// 난수(랜덤)
-	srand((unsigned int)time(NULL));
+	// <C++ 클래스>
+	// - 생성자, 소멸자: 객체 생성 및 소멸 시 자동으로 호출, 별도로 작성하지 않아도 컴파일러가 자동으로 만든다.
+	// - 멤버 함수: 해당 클래스가 사용하는 전용 함수, 이 함수는 객체(인스턴스)를 통해 호출할 수 있다.
+	CTest test1, test2, test3;
 
-	// 함수 포인터 - 반환 값 (*변수명) (매개변수)
-	void(*func)() = nullptr;
+	// 접근 제한 지정자가 private이므로, 외부에서 접근할 수 없다.
+	// test1.m_i = 10;
 
-	func = Test;
-	func();
+	// 따라서 Setter, Getter 함수를 이용하여 값을 변경하였다.
+	test1.SetInt(100);
 
-	int arr[10] = {};
+	// 대입 연산자 호출(별도로 만들지 않으면 컴파일러가 자동으로 생성)
+	(test3 = test2) = test1;
 
-	for (int i = 0; i < 10; ++i)
-	{
-		arr[i] = rand() % 100 + 1;
-	}
+	int a = 10;
 
-	printf("정렬 전: ");
-	for (int i = 0; i < 10; ++i)
-	{
-		printf("%d ", arr[i]);
-	}
+	// 1. 포인터 변수 선언: 자료형* 변수명;
+	// 2. 변수의 주소값: &변수;
+	int* p = &a;
 
-	Sort(arr, 10, InsertionSort);
+	printf("%d\n", a);
 
-	printf("정렬 후: ");
-	for (int i = 0; i < 10; ++i)
-	{
-		printf("%d ", arr[i]);
-	}
-}
+	// 3. 포인터로 주소 역참조: *변수명;
+	*p = 100;
 
-void Test()
-{
-	printf("Test()\n");
-}
+	printf("%d\n", a);
 
-void Sort(int* arr, int n, void(*f)(int*, int))
-{
-	f(arr, n);
-}
+	// 4. 래퍼런스 변수 선언: 자료형& 변수명;
+	// 포인터와 유사, 원본 객체를 참조한다.
+	// 역참조 없이 직접적으로 원본을 수정할 수 있다.
+	int& ref = a;
 
-void SelectionSort(int* arr, int n)
-{
-	printf("\n선택 정렬 수행 중...\n");
+	ref = 200;
 
-	for (int i = 0; i < n - 1; ++i)
-	{
-		int minIndex = i;
-
-		for (int j = i + 1; j < n; ++j)
-		{
-			if (arr[j] < arr[minIndex])
-			{
-				minIndex = j;
-			}
-		}
-
-		swap(arr[i], arr[minIndex]);
-	}
-}
-
-void InsertionSort(int* arr, int n)
-{
-	printf("\n삽입 정렬 수행 중...\n");
-
-	for (int i = 1, j = 0; i < n; ++i)
-	{
-		int key = arr[i];
-
-		for (j = i - 1; j >= 0 && arr[j] > key; --j)
-		{
-			arr[j + 1] = arr[j];
-		}
-
-		arr[j + 1] = key;
-	}
-}
-
-void BubbleSort(int* arr, int n)
-{
-	printf("\n버블 정렬 수행 중...\n");
-
-	for (int i = n - 1; i > 0; --i)
-	{
-		for (int j = 0; j < i; ++j)
-		{
-			if (arr[j] > arr[j + 1])
-			{
-				swap(arr[j], arr[j + 1]);
-			}
-		}
-	}
+	printf("%d\n", a);
 }
