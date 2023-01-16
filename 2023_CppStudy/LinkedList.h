f88e7539c19a20b717624615351f1ca20ce4711c
JongWoo <jinlaove17@naver.com>
2023-01-16 21:45:22 +0900

    [Add] list iterator, insert, erase

--- 2023_CppStudy/LinkedList.h
+++ 2023_CppStudy/LinkedList.h
@@ -1,10 +1,18 @@
 #pragma once
 
+#include <cassert>
+
+template<typename T>
+class CLinkedListIterator;
+
 template<typename T>
 class CNode
 {
 	template<typename T>
 	friend class CLinkedList;
+
+	template<typename T>
+	friend class CLinkedListIterator;
 
 private:
 	T      m_data;
@@ -13,20 +21,35 @@ private:
 	CNode* m_next;
 
 private:
-	CNode() : m_data(), m_prev(nullptr), m_next(nullptr) {}
+	CNode() :
+		m_data(),
+		m_prev(nullptr),
+		m_next(nullptr)
+	{
+
+	}
 };
 
 template<typename T>
 class CLinkedList
 {
+public:
+	using iterator = CLinkedListIterator<T>;
+
+	template<typename T>
+	friend class CLinkedListIterator;
+
 private:
-	unsigned int m_size;
+	int          m_size;
 
 	CNode<T>*    m_begin;
 	CNode<T>*    m_end;
 
 public:
-	CLinkedList() : m_size(0), m_begin(new CNode<T>), m_end(new CNode<T>)
+	CLinkedList() :
+		m_size(0),
+		m_begin(new CNode<T>),
+		m_end(new CNode<T>)
 	{
 		m_begin->m_next = m_end;
 		m_end->m_prev = m_begin;
@@ -40,9 +63,19 @@ public:
 		delete m_end;
 	}
 
-	unsigned int size()
+	int size()
 	{
 		return m_size;
+	}
+
+	iterator begin()
+	{
+		return iterator(this, m_begin->m_next);
+	}
+
+	iterator end()
+	{
+		return iterator(this, m_end);
 	}
 
 	void push_front(const T& data)
@@ -77,6 +110,56 @@ public:
 		++m_size;
 	}
 
+	iterator insert(const iterator& iter, const T& data)
+	{
+		assert(end() != iter);
+
+		CNode<T>* newNode = new CNode<T>;
+
+		newNode->m_data = data;
+
+		// iter 반복자 한 칸 앞에 data를 가진 노드를 삽입한다.
+
+		// iter가 이미 맨 앞 노드인 경우
+		if (iter.m_node == m_begin->m_next)
+		{
+			m_begin->m_next = newNode;
+			newNode->m_prev = m_begin;
+			newNode->m_next = iter.m_node;
+			iter.m_node->m_prev = newNode;
+		}
+		else
+		{
+			iter.m_node->m_prev->m_next = newNode;
+			newNode->m_prev = iter.m_node->m_prev;
+			newNode->m_next = iter.m_node;
+			iter.m_node->m_prev = newNode;
+		}
+
+		++m_size;
+
+		return iterator(this, newNode);
+	}
+
+	iterator erase(iterator& iter)
+	{
+		assert(end() != iter);
+
+		CNode<T>* deletedNode = iter.m_node;
+
+		deletedNode->m_prev->m_next = deletedNode->m_next;
+		deletedNode->m_next->m_prev = deletedNode->m_prev;
+
+		CNode<T>* nextNode = deletedNode->m_next;
+
+		delete deletedNode;
+
+		iter.m_isValid = false;
+		--m_size;
+
+		return iterator(this, nextNode);
+	}
+
 	void clear()
 	{
 		CNode<T>* node = m_begin->m_next;
@@ -94,5 +177,80 @@ public:
 
 		m_begin->m_next = m_end;
 		m_end->m_prev = m_begin;
+	}
+};
+
+template<typename T>
+class CLinkedListIterator
+{
+	template<typename T>
+	friend class CLinkedList;
+
+private:
+	CLinkedList<T>* m_linkedList;
+	CNode<T>*       m_node;
+
+	bool			m_isValid;
+
+public:
+	CLinkedListIterator(CLinkedList<T>* linkedList, CNode<T>* node) :
+		m_linkedList(linkedList),
+		m_node(node)
+	{
+		if (nullptr != linkedList && nullptr != node)
+		{
+			m_isValid = true;
+		}
+	}
+
+	T& operator *()
+	{
+		return m_node->m_data;
+	}
+
+	bool operator ==(const CLinkedListIterator<T>& other)
+	{
+		return (m_linkedList == other.m_linkedList) && (m_node == other.m_node);
+	}
+
+	bool operator !=(const CLinkedListIterator<T>& other)
+	{
+		return !(*this == other);
+	}
+
+	CLinkedListIterator<T>& operator ++()
+	{
+		assert(m_node != m_linkedList->m_end && m_isValid);
+
+		m_node = m_node->m_next;
+
+		return *this;
+	}
+
+	CLinkedListIterator<T> operator ++(int)
+	{
+		CLinkedListIterator<T> iter(*this);
+
+		++(*this);
+
+		return iter;
+	}
+
+	CLinkedListIterator<T>& operator --()
+	{
+		assert(m_node != m_linkedList->m_begin && m_isValid);
+
+		m_node = m_node->m_prev;
+
+		return *this;
+	}
+
+	CLinkedListIterator<T> operator --(int)
+	{
+		CLinkedListIterator<T> iter(*this);
+
+		--(*this);
+
+		return iter;
 	}
 };
--- 2023_CppStudy/main.cpp
+++ 2023_CppStudy/main.cpp
@@ -1,71 +1,29 @@
 #include <iostream>
 
-#include "DynamicArray.h"
+#include "LinkedList.h"
 
 using namespace std;
 
 int main()
 {
-	CDynamicArray<int> dynamicArray;
+	CLinkedList<int> linkedList;
 
-	dynamicArray.push_back(10);
-	dynamicArray.push_back(20);
-	dynamicArray.push_back(30);
+	linkedList.push_back(10);
+	linkedList.push_back(20);
+	linkedList.push_back(30);
 
-	// 직접 만든 이터레이터를 사용해보자.
-	CDynamicArray<int>::iterator iter = dynamicArray.begin();
+	CLinkedList<int>::iterator iter = linkedList.begin();
 
-	dynamicArray.erase(iter);
+	linkedList.insert(++iter, 15);
+	iter = linkedList.insert(linkedList.begin(), 5);
 
-	// 무효한 반복자 접근 방지 구현
-	// int k = *iter;
+	linkedList.erase(++linkedList.begin());
 
-	for (iter = dynamicArray.begin(); iter != dynamicArray.end(); iter++)
+	cout << linkedList.size() << endl;
+
+	for (; iter != linkedList.end(); ++iter)
 	{
 		cout << *iter << ' ';
 	}
 	cout << endl;
-
-	//{
-	//	// 직접 만든 이터레이터를 사용해보자.
-	//	CDynamicArray<int>::iterator iter = dynamicArray.begin();
-
-	//	cout << *(iter++) << endl;
-	//	cout << *(--iter) << endl;
-	//	cout << *(++iter) << endl;
-	//	cout << *(iter++) << endl;
-	//	cout << *iter << endl;
-
-	//	// 연산자 우선순위에 의하여 아래 구문은 위와 다른 결과를 출력할 것이다.
-	//	//cout << *(iter++) << ' ' << *(--iter) << ' ' << *(++iter) << ' ' << *(iter++) << ' ' << *iter << endl;
-	//}
-
-	// erase 사용시 주의할 예시
-	dynamicArray.clear();
-
-	// 1 ~ 10으로 채운다.
-	for (int i = 0; i < 10; ++i)
-	{
-		dynamicArray.push_back(i + 1);
-	}
-
-	// 짝수만 제거한다.
-	for (iter = dynamicArray.begin(); iter != dynamicArray.end();)
-	{
-		if (0 == *iter % 2)
-		{
-			iter = dynamicArray.erase(iter);
-		}
-		else
-		{
-			++iter;
-		}
-	}
-
-	for (int i = 0; i < dynamicArray.size(); ++i)
-	{
-		cout << dynamicArray[i] << ' ';
-	}
-	cout << "\nsize: " << dynamicArray.size() << endl;
-	cout << "capacity: " << dynamicArray.capacity() << endl;
 }
